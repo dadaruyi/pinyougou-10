0467cd005adc355c317d10894cebc8f4f7be796a
dada <dadariyi@.com>
2019-07-03 01:34:20 +0800

    pinyougou-parent10.2

--- pinyougou-parent/pinyougou-search-service/src/main/java/com/pinyougou/search/service/impl/ItemSearchServiceImpl.java
+++ pinyougou-parent/pinyougou-search-service/src/main/java/com/pinyougou/search/service/impl/ItemSearchServiceImpl.java
@@ -5,13 +5,14 @@ import com.pinyougou.pojo.TbItem;
 import com.pinyougou.pojo.TbItemExample;
 import com.pinyougou.search.service.ItemSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.solr.core.SolrTemplate;
-import org.springframework.data.solr.core.query.Criteria;
-import org.springframework.data.solr.core.query.Query;
-import org.springframework.data.solr.core.query.SimpleQuery;
-import org.springframework.data.solr.core.query.result.ScoredPage;
+import org.springframework.data.solr.core.query.*;
+import org.springframework.data.solr.core.query.result.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 @Service(timeout = 5000)
 public class ItemSearchServiceImpl implements ItemSearchService {
@@ -20,13 +21,72 @@ public class ItemSearchServiceImpl implements ItemSearchService {
     @Override
     public Map<String, Object> search(Map searchMap) {
         Map<String,Object> map=new HashMap<>();
-        Query query=new SimpleQuery();
-        //添加条件
-        Criteria criteria = new Criteria("item_keywords").is(searchMap.get("keywords"));
-        query.addCriteria(criteria);
-        ScoredPage<TbItem> page = solrTemplate.queryForPage(query, TbItem.class);
-        map.put("rows",page.getContent());
-
+       //1.查询列表
+        map.putAll(searchList(searchMap));
+        //2.根据关键字查询商品分类
+        List categoryList = searchCategoryList(searchMap);
+        map.put("categoryList",categoryList);
         return map;
     }
+
+    /**
+     * 根据关键字查询列表
+     * @param searchMap
+     * @return
+     */
+    private Map searchList(Map searchMap){
+    Map map=new HashMap();
+        HighlightQuery query = new SimpleHighlightQuery();
+        //设置高亮显示的域
+        HighlightOptions highlightOptions = new HighlightOptions().addField("item_title");
+        //高亮的前缀
+        highlightOptions.setSimplePrefix("<em style='color:red'>");
+        //高亮的后缀
+        highlightOptions.setSimplePostfix("</em>");
+        //设置高亮选项
+        query.setHighlightOptions(highlightOptions);
+        //按照关键字查询
+        Criteria criteria = new Criteria("item_keywords").is(searchMap.get("keywords"));
+        query.addCriteria(criteria);
+        HighlightPage<TbItem> page = solrTemplate.queryForHighlightPage(query, TbItem.class);
+        //循环高亮入口集合
+        for (HighlightEntry<TbItem> h : page.getHighlighted()) {
+            TbItem item = h.getEntity();//获取源实体类
+            if (h.getHighlights().size()>0&&h.getHighlights().get(0).getSnipplets().size()>0){
+                //设置高亮的结果
+                item.setTitle(h.getHighlights().get(0).getSnipplets().get(0));
+            }
+
+        }
+        map.put("rows",page.getContent());
+        return map;
+    }
+    /**
+     * 查询分类列表
+     * @param searchMap
+     * @return
+     */
+    private  List searchCategoryList(Map searchMap){
+        List<String> list=new ArrayList();
+        Query query=new SimpleQuery();
+        //按照关键字查询
+        Criteria criteria=new Criteria("item_keywords").is(searchMap.get("keywords"));
+        query.addCriteria(criteria);
+        //设置分组选项
+        GroupOptions groupOptions=new GroupOptions().addGroupByField("item_category");
+        query.setGroupOptions(groupOptions);
+        //得到分组页
+        GroupPage<TbItem> page = solrTemplate.queryForGroupPage(query, TbItem.class);
+        //根据列得到分组结果集
+        GroupResult<TbItem> groupResult = page.getGroupResult("item_category");
+        //得到分组结果入口页
+        Page<GroupEntry<TbItem>> groupEntries = groupResult.getGroupEntries();
+        //得到分组入口集合
+        List<GroupEntry<TbItem>> content = groupEntries.getContent();
+        for(GroupEntry<TbItem> entry:content){
+            list.add(entry.getGroupValue());//将分组结果的名称封装到返回值中
+        }
+        return list;
+    }
+
 }
