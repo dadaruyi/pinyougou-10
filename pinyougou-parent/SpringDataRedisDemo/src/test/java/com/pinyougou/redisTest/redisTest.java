da62751d22357c2e61508925a947939047ce60ae
dada <dadariyi@.com>
2019-07-03 01:44:05 +0800

    pinyougou-parent10.2

---
+++ pinyougou-parent/SpringDataRedisDemo/src/test/java/com/pinyougou/redisTest/redisTest.java
@@ -0,0 +1,28 @@
+package com.pinyougou.redisTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:spring/applicationContext-redis.xml")
+public class redisTest {
+    @Autowired
+    private RedisTemplate redisTemplate;
+    @Test
+    public void setValue(){
+        redisTemplate.boundValueOps("name").set("haha");
+    }
+    @Test
+    public void getValue(){
+        String name = (String) redisTemplate.boundValueOps("name").get();
+        System.out.println(name);
+    }
+    @Test
+    public void deleteValue(){
+        redisTemplate.delete("name");
+    }
+}
